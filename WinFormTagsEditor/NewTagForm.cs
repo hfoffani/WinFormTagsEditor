616353e3abcd32042ee852c907daa23a9db1bbc3
Hernan Foffani <hfoffani@gmail.com>
2015-12-03 17:04:23 +0100

    watermark for add tag.

--- WinFormTagsEditor/NewTagForm.cs
+++ WinFormTagsEditor/NewTagForm.cs
@@ -7,24 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Runtime.InteropServices;
+
 namespace WFTE
 {
     internal partial class NewTagForm : Form
     {
+        string waterMarkText = "<Enter> to add. <Esc> to close.";
+        Font prototype;
+
         public NewTagForm()
         {
             InitializeComponent();
+            prototype = this.textBox1.Font;
+            this.textBox1.Font = new Font(prototype.FontFamily, prototype.Size-2, prototype.Style);
             this.btnok.Width = 0;
             this.btncancel.Width = 0;
             this.textBox1.TextChanged += textBox1_TextChanged;
+
+            SendMessage(this.textBox1.Handle, 0x1501, 1, waterMarkText);
         }
 
 
         void textBox1_TextChanged(object sender, EventArgs e)
         {
             this.Value = this.textBox1.Text;
+
+            if (this.textBox1.Text != "") {
+                this.textBox1.Font = prototype;
+            } else {
+                this.textBox1.Font = new Font(prototype.FontFamily, prototype.Size - 2, prototype.Style);
+            }
         }
 
         public string Value { get; set; }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
     }
 }
